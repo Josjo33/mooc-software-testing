2467151e1a5c3047581ee0905295710e34801806
Josjo33 <joshuahugo131@gmail.com>
2025-03-28 14:44:18 -0500

    Actualiza valores de prueba en onlyBigBars en ChocolateBagsTest.java

--- src/test/java/tudelft/chocolate/ChocolateBagsTest.java
+++ src/test/java/tudelft/chocolate/ChocolateBagsTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.params.provider.CsvSource;
 public class ChocolateBagsTest {
     @ParameterizedTest(name = "small={0}, big={1}, total={2}, result={3}")
     @CsvSource({ "1,1,5,0", "1,1,6,1", "1,1,7,-1", "1,1,8,-1" })
+
     public void totalIsTooBig(int small, int big, int total, int expectedResult) {
         ChocolateBags bags = new ChocolateBags();
         int result = bags.calculate(small, big, total);
@@ -14,7 +15,8 @@ public class ChocolateBagsTest {
     }
 
     @ParameterizedTest(name = "small={0}, big={1}, total={2}, result={3}")
-    @CsvSource({ "4,0,10,-1", "4,1,10,-1", "5,2,10,0", "5,3,10,0" })
+    @CsvSource({ "0,2,10,0", "0,3,15,0", "0,1,6,-1", "0,2,11,-1" })
+
     public void onlyBigBars(int small, int big, int total, int expectedResult) {
         int result = new ChocolateBags().calculate(small, big, total);
         Assertions.assertEquals(expectedResult, result);
